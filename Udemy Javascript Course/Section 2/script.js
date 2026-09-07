042997adee74c92289edf0fcf75cd39c4cd01740
Sam Darr <{sam.darr@outlook.com}>
2026-09-07 10:29:56 -0400

    Add initial HTML structure and expand JavaScript functionality with variable declarations and type conversions

--- Udemy Javascript Course/Section 2/script.js
+++ Udemy Javascript Course/Section 2/script.js
@@ -1,9 +1,32 @@
-console.log(100);
-console.log('Hello World');
-console.log(20, 'Hello', true);
-const x = 100;
-console.error('Alert');
-console.warn('Warning');
-console.table
-console.group
-console.groupEnd
+
+
+
+
+//NaN 
+
+//Operators are symbols that can be used in expressions to perform certain tasks
+//Arithmetic
+let z;
+console.log(z);
+
+//concatenation 
+//combines strings together
+
+//Assignment operators
+x = 10;
+x += 5;
+console.log(x);
+
+/*comparison operators
+x = 2 == '2';
+x = 2 == '2';
+x = 2 != 2; *\
+
+console.log(x);
+
+/*Type coercion
+let y;
+y= 5 + '5';*\
+
+
+//Strings

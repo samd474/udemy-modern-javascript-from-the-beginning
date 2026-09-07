c77fffe8a326f3590acb5c29e790f4b71dfa14c5
Sam Darr <{sam.darr@outlook.com}>
2026-09-07 10:59:09 -0400

    Add HTML structure and JavaScript files for number and string manipulations

---
+++ Udemy Javascript Course/Section 1/Numbers/script.js
@@ -0,0 +1,8 @@
+let x;
+const num = new Number(5);
+x = num.toString();
+x = num.toString.length;
+x = num.toFixed(2);
+x = num.toPrecision(3);
+x = num.toExponential(2);
+console.log(x);
--- Udemy Javascript Course/Strings/script.js
+++ Udemy Javascript Course/Strings/script.js
@@ -19,4 +19,23 @@ x = typeof s;
 x = s[0];
 //access value by key
 
+//prototypes are where methods are stored
 x = s.__proto__;
+
+x = s.toUpperCase;
+x=s.toLowerCase;
+
+x = s.substring;
+
+x = s.substring(7);
+
+x = '          Hello, World'
+x = x.trim;
+
+x = s.replace('World', 'John');
+
+
+x = s.split;
+
+const myString = 'developer';
+let newString = myString.toUpperCase(0)
